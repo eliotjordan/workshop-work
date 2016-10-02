1e451f8b8d8fe89bd0b96e8dc821683a76920a8c
Eliot Jordan <eliot.jordan@gmail.com>
2016-10-02 19:05:27 -0400

    Update messaging-client.rb

--- messaging-client.rb
+++ messaging-client.rb
@@ -2,11 +2,6 @@ require 'bunny'
 
 module GeoConcerns
   class RabbitMessagingClient
-    attr_reader :amqp_url
-    def initialize(amqp_url)
-      @amqp_url = amqp_url
-    end
-
     def publish(message)
       exchange_type = JSON.parse(message)['exchange']
       send(exchange_type)
@@ -24,15 +19,7 @@ module GeoConcerns
       exchange_name = Messaging.config['events']['exchange']['geoserver']
       @exchange ||= channel.fanout(exchange_name, durable: true)
     end
-
-    private
-
-      def bunny_client
-        @bunny_client ||= Bunny.new(amqp_url).tap(&:start)
-      end
-
-      def channel
-        @channel ||= bunny_client.create_channel
-      end
+    
+    ...
   end
 end
